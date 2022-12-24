cc6c2308f4fc38b5f31e7213ddaaa49f6d172259
Sudarshan Kondgekar <skondgekar@yahoo.co.in>
2022-12-24 21:12:39 +0000

    Correctly exporting all models

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import { IUser, userModel, userSchema } from "./src/models/user";
+import { IApps, appsModel } from "./src/models/apps";
+import * as customTypes from "./src/objects/customTypes";
 
 export { IUser, userModel, userSchema };
+
+export { IApps, appsModel };
+
+export { customTypes };
